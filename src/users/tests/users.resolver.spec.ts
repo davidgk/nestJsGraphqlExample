55bfb6431def6f09cf98446ca225c37aa552b88a
David Kotlirevsky <david.kotlirevsky@gmail.com>
2022-12-01 19:26:40 -0300

    Fix test users

--- src/users/tests/users.resolver.spec.ts
+++ src/users/tests/users.resolver.spec.ts
@@ -1,16 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersResolver } from '@user/resolver/users.resolver';
+import { TestTypeOrmModules } from '@database/test-database-helper';
+import { User } from '@user/models/entities/user.entity';
 import { UsersService } from '@user/service/users.service';
+import { UserRepository } from '@user/repositories/user.repository';
 
 describe('UsersResolver', () => {
   let resolver: UsersResolver;
+  let moduleRef: TestingModule;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersResolver, UsersService],
+    moduleRef = await Test.createTestingModule({
+      imports: [...TestTypeOrmModules(User)],
+      providers: [UsersResolver, UsersService, UserRepository],
     }).compile();
 
-    resolver = module.get<UsersResolver>(UsersResolver);
+    resolver = moduleRef.get<UsersResolver>(UsersResolver);
   });
 
   it('should be defined', () => {
--- src/users/tests/users.service.spec.ts
+++ src/users/tests/users.service.spec.ts
@@ -1,16 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from '@user/service/users.service';
-import { UsersModule } from '@user/users.module';
+import { TestTypeOrmModules } from '@database/test-database-helper';
+import { User } from '@user/models/entities/user.entity';
+import { UserRepository } from '@user/repositories/user.repository';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let moduleRef: TestingModule;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      imports: [UsersModule],
+    moduleRef = await Test.createTestingModule({
+      imports: [...TestTypeOrmModules(User)],
+      providers: [UsersService, UserRepository],
     }).compile();
-
-    service = module.get<UsersService>(UsersService);
+    service = moduleRef.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
